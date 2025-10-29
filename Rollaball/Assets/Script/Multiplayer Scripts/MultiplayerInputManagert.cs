48153714f5e7348857b8e719faf98bb58ac99397
Andy <andyvoong168@gmail.com>
2025-10-29 11:50:23 -0700

    Multiplayer Scene

---
+++ Rollaball/Assets/Script/Multiplayer Scripts/MultiplayerInputManagert.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MultiplayerInputManagert : MonoBehaviour
+{
+   [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Transform[] spawnPoints;
+
+    private bool wasdJoined = false;
+    private bool arrowsJoined = false;
+    private bool gamepadJoined = false;
+
+    void Update()
+    {
+        if (Keyboard.current == null) return;
+
+        if (!wasdJoined && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            var player = PlayerInput.Instantiate(playerPrefab,
+                controlScheme: "WASD",
+                pairWithDevice: Keyboard.current);
+
+            if (spawnPoints.Length > 0)
+            {
+                player.transform.position = spawnPoints[0].position;
+            }
+
+            wasdJoined = true;
+        }
+
+        if (!arrowsJoined && Keyboard.current.rightCtrlKey.wasPressedThisFrame)
+        {
+            var player = PlayerInput.Instantiate(playerPrefab,
+                controlScheme: "Arrows",
+                pairWithDevice: Keyboard.current);
+
+            if (spawnPoints.Length > 1)
+            {
+                player.transform.position = spawnPoints[1].position;
+            }
+
+            arrowsJoined = true;
+        }
+
+        foreach (var gamePad in Gamepad.all)
+        {
+            if (gamePad.buttonSouth.wasPressedThisFrame && !gamepadJoined)
+            {
+                var player = PlayerInput.Instantiate(playerPrefab,
+                    controlScheme: "Gamepad",
+                    pairWithDevice: gamePad);
+
+                gamepadJoined = true;
+            }
+        }
+
+    }
+
+}

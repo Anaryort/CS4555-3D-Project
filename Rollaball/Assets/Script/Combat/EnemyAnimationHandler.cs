067f7fac776b06413dd64073185a570e5ff300bb
Troy Rana <121784171+Anaryort@users.noreply.github.com>
2025-11-03 18:24:45 -0800

    Enemy Animation Handler Update (Melee only for now)

---
+++ Rollaball/Assets/Script/Combat/EnemyAnimationHandler.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Enemy_Ai_Melee))]
+public class EnemyAnimationHandler : MonoBehaviour
+{
+    private Animator animator;
+    private Enemy_Ai_Melee enemyAI;
+    private NavMeshAgent agent;
+
+    private bool isIdle;
+    private bool isWalking;
+    private bool isAttacking;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        enemyAI = GetComponent<Enemy_Ai_Melee>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (enemyAI == null || animator == null)
+            return;
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        bool walking = agent.velocity.magnitude > 0.1f && !enemyAI.playerInAttackRange;
+        bool attacking = enemyAI.playerInAttackRange && enemyAI.playerInSightRange;
+        bool idle = !walking && !attacking;
+
+        if (walking != isWalking || attacking != isAttacking || idle != isIdle)
+        {
+            isWalking = walking;
+            isAttacking = attacking;
+            isIdle = idle;
+
+            animator.SetBool("isWalking", isWalking);
+            animator.SetBool("isAttacking", isAttacking);
+            animator.SetBool("isIdle", isIdle);
+        }
+    }
+}
